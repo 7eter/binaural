0fa2252c3317762af805f8697bccdaba7c69e606
Peter <peteramried@web.de>
2022-12-21 21:10:57 +0100

    add panner nodes for stereo effect

--- script.js
+++ script.js
@@ -31,15 +31,23 @@ playButton.addEventListener('click', () => {
   const differenceFrequency = 10; // This is the difference between the left and right channel frequencies, in Hz
   leftChannel.frequency.setValueAtTime(baseFrequencyInput.value - differenceFrequencyInput.value, audioCtx.currentTime);
   rightChannel.frequency.setValueAtTime(parseFloat(baseFrequencyInput.value) + parseFloat(differenceFrequencyInput.value), audioCtx.currentTime);
+  const pannerLeft = audioCtx.createPanner();
+  const pannerRight = audioCtx.createPanner();
   gainNode.gain.setValueAtTime(volumeInput.value, audioCtx.currentTime);
+
+  // Set the panning of the channels
+  pannerLeft.pan.setValueAtTime(-1, audioCtx.currentTime); // Pan the left channel to the left speaker
+  pannerRight.pan.setValueAtTime(1, audioCtx.currentTime); // Pan the right channel to the right speaker
   
   console.log("base frequency = " + baseFrequencyInput.value)
   console.log("difference frequency = " + differenceFrequencyInput.value)
   console.log("left channel frequency = " + leftChannel.frequency.value)
   console.log("right channel frequency = " + rightChannel.frequency.value)
   // Connect the nodes
-  leftChannel.connect(gainNode);
-  rightChannel.connect(gainNode);
+  leftChannel.connect(pannerLeft);
+  rightChannel.connect(pannerRight);
+  pannerLeft.connect(gainNode);
+  pannerRight.connect(gainNode);
   gainNode.connect(audioCtx.destination);
 
 
