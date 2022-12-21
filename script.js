6904bc9354ac9c8de77eac57de7ba0b175c5ae8f
7eter <peteramried@web.de>
2022-12-21 01:09:24 +0100

    fixed calculation for right channel freqency

--- script.js
+++ script.js
@@ -31,9 +31,13 @@ playButton.addEventListener('click', () => {
   const baseFrequency = 200; // This is the base frequency, in Hz
   const differenceFrequency = 10; // This is the difference between the left and right channel frequencies, in Hz
   leftChannel.frequency.setValueAtTime(baseFrequencyInput.value - differenceFrequencyInput.value, audioCtx.currentTime);
-  rightChannel.frequency.setValueAtTime(baseFrequencyInput.value + differenceFrequencyInput.value, audioCtx.currentTime);
+  rightChannel.frequency.setValueAtTime(parseFloat(baseFrequencyInput.value) + parseFloat(differenceFrequencyInput.value), audioCtx.currentTime);
   gainNode.gain.setValueAtTime(volumeInput.value, audioCtx.currentTime);
-
+  
+  console.log("base frequency = " + baseFrequencyInput.value)
+  console.log("difference frequency = " + differenceFrequencyInput.value)
+  console.log("left channel frequency = " + leftChannel.frequency.value)
+  console.log("right channel frequency = " + rightChannel.frequency.value)
   // Connect the nodes
   leftChannel.connect(gainNode);
   rightChannel.connect(gainNode);
